83d08382eb67cf8b2a58d505878f263c187956cb
danfrz <dansaoficial@gmail.com>
2017-02-14 18:29:19 -0600

    Changes TodoList to be colored by category

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -3,16 +3,14 @@ import { TodoListService } from "./todo-list.service";
 import { Todo } from "./todo";
 import { FilterBy } from "./filter.pipe";
 
-
 @Component({
     selector: 'todo-list-component',
     templateUrl: 'todo-list.component.html',
     providers: [FilterBy],
 })
 
-
-
 export class TodoListComponent implements OnInit {
+    static ColorBinds = new Array<string>();
     public todos: Todo[];
 
     constructor(private todoListService: TodoListService) {
@@ -26,6 +24,25 @@ export class TodoListComponent implements OnInit {
             }
         );
     }
+
+    public static getRandomColor() : string
+    {
+        return "hsl(" + Math.round(359*Math.random())+",40%,81%)";
+    }
+
+    public getColor(category : string) : string
+    {
+        var clrBinding:string = TodoListComponent.ColorBinds[category];
+        if(clrBinding == null) // If this category doesnt exist in the mapping, grab a new random color
+        {
+            TodoListComponent.ColorBinds[category] = TodoListComponent.getRandomColor();
+            return TodoListComponent.ColorBinds[category];
+        }
+        else
+            return clrBinding;
+    }
+
+
 }
 
 
